529303993dc30ac0cd2cde26884e53f200de4a3e
Wellington <wgn1910ww@gmail.com>
2024-02-27 21:22:37 -0300

    edição da aula9

--- arquivos/aula9.py
+++ arquivos/aula9.py
@@ -14,4 +14,11 @@ print("divisão", divisao)
 divisao_inteira = 10 // 2
 print("Divisão inteira", divisao_inteira)
 
-modulo = 55% 2
+modulo = 13 % 4
+print('modulo', modulo) #resto da divisão
+
+print(10%3 == 0)
+print(10%2 == 0)
+print(10%5 == 0)
+print(10%4 == 0)
+
