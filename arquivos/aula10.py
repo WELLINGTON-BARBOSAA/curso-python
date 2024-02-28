c5cbbec4e90b748015f23e365326ffd95b59a6c3
Wellington <wgn1910ww@gmail.com>
2024-02-27 21:38:19 -0300

    Aula sobre outras funções do sinal de + e *

---
+++ arquivos/aula10.py
@@ -0,0 +1,10 @@
+''' Sinal de + também tem a função tambem (dependendo do tipo) ele faz a concatenação '''
+
+concatenacao = 'Wellington' + ' ' 'Gomes' + ' ' 'de' + ' ' 'Sousa' + ' ' 'Barbosa'
+print(concatenacao)
+
+"""A multiplicação * tambem tem a função de repetir uma str a quantidade de vez que quise """
+Wellington_5_vezes = 5 * 'Wellington'
+G_3_vezes = 3 * 'G'
+print(Wellington_5_vezes)
+print(G_3_vezes)
