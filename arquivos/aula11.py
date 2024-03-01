d0cc88129b182b85e1eab1b07bcea18c6bb34698
Wellington <wgn1910ww@gmail.com>
2024-02-29 21:57:07 -0300

    Exercicio sobre IMC

---
+++ arquivos/aula11.py
@@ -0,0 +1,13 @@
+# Ordem dos operadores 
+# 1. (n + n) -> na programação é dado prioridade aos parenteses ()
+# 2. ** exponenciação 
+# 3. */ // %
+# 4. + -
+
+conta_1 = (5 + 5) // (1 + 1) 
+conta_2 = (1 + 1) ** (5 + 5) 
+conta_3 = (5 + 5) - (1 + 1) 
+
+print(conta_1)
+print(conta_2)
+print(conta_3)
---
+++ arquivos/aula12.py
@@ -0,0 +1,10 @@
+nome = "Wellington Gomes"
+altura = 1.70
+peso = 78 
+imc = peso / (altura*altura)
+
+print("Nome do paciente:", nome)
+print("Altura do paciente:", altura)
+print("Seu peso atual:", peso)
+print("Fazendo os devidos calculos, seu imc é de:", imc)
+print("Então o paciente se encontra em Sobrepeso")
