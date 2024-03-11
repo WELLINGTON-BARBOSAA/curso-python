08b2a1700a40e321fc032af5d1772fafb3477c6e
Wellington <wgn1910ww@gmail.com>
2024-03-11 18:32:32 -0300

    aula if, elif e else

---
+++ aula16.py
@@ -0,0 +1,14 @@
+# if / elif     /else
+# se / se não se/ se não
+
+entrada = input('você quer "entrar" ou "sair"?')
+
+if entrada == "entrar":
+    print("voce entrou no sistema")
+    print("Parabéns vc acabou de entrar no sistema")
+
+elif entrada == "sair":
+    print("Você saiu do sistema")
+    print("Você acabou de sair do sistema")
+else:
+    print("Voce não digitou nenhuma das opções")
