69ee098cae3b9db38cdf9a2ff66b4b23d53150a8
Wellington <wgn1910ww@gmail.com>
2024-03-12 21:00:10 -0300

    revisão aula16

--- arquivos/aula16.py
+++ arquivos/aula16.py
@@ -1,14 +1,15 @@
 # if / elif     /else
 # se / se não se/ se não
 
-entrada = input('você quer "entrar" ou "sair"?')
+entrada = input ("Voce deseja 'entrar' ou 'sair' do sistema?")
 
 if entrada == "entrar":
-    print("voce entrou no sistema")
-    print("Parabéns vc acabou de entrar no sistema")
+    print("Voce acabou de entrar no sistema")
+    print("Aguarde, enquanto o sistema esta carregando")
 
 elif entrada == "sair":
-    print("Você saiu do sistema")
-    print("Você acabou de sair do sistema")
+    print("Voce acabou de sair do sistema")
+    print("Aguarde, enquanto o sistema é encerrado")
+
 else:
-    print("Voce não digitou nenhuma das opções")
+    print("Voce não digitou nada ate o momento")
