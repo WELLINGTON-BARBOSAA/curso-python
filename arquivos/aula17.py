8beb7b814a6d87d3a78a95a42a4022abcf5b01e8
Wellington <wgn1910ww@gmail.com>
2024-03-12 21:17:04 -0300

    continuação if,elif e else

---
+++ arquivos/aula17.py
@@ -0,0 +1,28 @@
+# if / elif     /else
+# se / se não se/ se não
+"""No Boolean, só será executado o que for verdadeiro"""
+condição1 = False
+condição2 = False
+condição3 = False
+condição4 = False
+condição5 = False
+
+if condição1:
+    print("Condição 1 é falsa")
+
+elif condição2:
+    print("Condição 2 é falsa")
+
+elif condição3:
+    print("Condição 3 é falsa")
+
+elif condição4:
+    print("Condição 4 é falsa")
+
+elif condição5:
+    print("Esta condição é Verdadeira")
+
+else:
+    print("nenhuma das condições foram executadas")
+    print("Sair dos sistema")
+    
