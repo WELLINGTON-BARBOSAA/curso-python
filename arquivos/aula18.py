7ae1c524478f467e66d9c983fe1c7784a507c987
Wellington <wgn1910ww@gmail.com>
2024-03-18 19:34:33 -0300

    Aula sobre DEbugger

---
+++ arquivos/aula18.py
@@ -0,0 +1,38 @@
+"""Aula sobre como funciona o Debugger.
+Sua função serve para aprender como esta funcionando seu codigo de cima para baixo da esquerda para direita, o fluxo. 
+Ele primeiro faz a leitura das condições existente no codigo.
+Depois de fazer a leitura das condições o Debugger vai executar o primeiro IF e suas 
+condições até achar a verdadeira, ao encontrar a mesma ele não tem mais o que executar 
+e vai para o proximo IF. Se caso ele não encontrar nenhuma condição verdadeira o Debugger
+vai passando até o outro IF existente no codigo"""
+
+
+condição1 = False
+condição2 = False
+condição3 = True
+condição4 = False
+condição5 = False
+
+if condição1:
+    print("Condição 1 é falsa")
+
+elif condição2:
+    print("Condição 2 é falsa")
+
+elif condição3:
+    print("Condição 3 é verdadeira")
+
+elif condição4:
+    print("Condição 4 é falsa")
+
+elif condição5:
+    print("Esta condição é Verdadeira")
+
+else:
+    print("nenhuma das condições foram executadas")
+    print("Sair dos sistema")
+
+if 10 == 10:
+    print("outro if")
+
+print("Fora do if")
