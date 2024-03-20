f37cf47e5cfe26e74504a67136ff2a1fc3266c99
Wellington <wgn1910ww@gmail.com>
2024-03-19 21:40:02 -0300

    Atividade incompleta if, elif e else

---
+++ arquivos/aula20atv.py
@@ -0,0 +1,16 @@
+primeiro_numero = input('Digite um numero: ')
+segundo_numero = input('Digite outro numero: ')
+
+if  primeiro_numero >= segundo_numero:  
+    print("O primeiro numero é maior que o segundo")
+elif primeiro_numero <= segundo_numero: 
+    print("O primeiro numero é menor que o segundo")
+else:
+    print("Voce não digitou nada até o momento")
+
+
+
+
+# Objetivo: Mostrar se o numero foi maior primeiro
+# se for menor mostrar segund
+    
