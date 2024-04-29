005ca7ecb0c19749813c22a47701bd297b95f188
Wellington <wgn1910ww@gmail.com>
2024-04-29 20:28:07 -0300

    revendo aulas

--- arquivos/aula2.py
+++ arquivos/aula2.py
@@ -1,5 +1,5 @@
 """Aula sobre a função print
-1- Na separação de como mostra no printa abaixo, a separação é feito com a utilização da virgula
+1- Na separação de como mostra no print abaixo, a separação é feito com a utilização da virgula
 2- Função sep, serve para utilizar um caractere como separador 
 3- Comando end, serve para adicionar uma quebra de linha de acordo com o caractere que o usuario
 quiser, ex :  print(12, 34, sep=" - ", end="\n##") -> nesse comando vai  a ver quebra de linha
@@ -8,8 +8,11 @@ jogando o caractere para linha de baixo ##56 - 78 ou se fosse print(12, 34, sep=
 
 """
 
-print(12, 34, sep=" - ", end="\n##")
+print(12, 34, sep=" - ", end="\n")
 print(56, 78, sep=" - ", end="\n")
 
 print(9, 10, sep=" / ")
 print(11, 12, sep="        ")
+print(25,25, sep="----------")
+print(14,18, sep="********")
+
