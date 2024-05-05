de536576ba6f877022268eda008338fe660a92c5
Wellington <wgn1910ww@gmail.com>
2024-05-05 19:25:28 -0300

    revisão aula7

--- arquivos/aula7.py
+++ arquivos/aula7.py
@@ -5,14 +5,14 @@
 # atribuir um valor a um nome (variavel).
 # Uso: nome_variavel = expressão
 
-""" local_onde_reside = "São João do Piauí"
+"""local_onde_reside = "São João do Piauí"
 data_nascimento = "15/03/1995"
 soma_idade_wellington = 27 + 2
 nome_completo = "Wellington Gomes de Sousa Barbosa"
 print("Qual é o seu nome completo:", nome_completo)
 print("Qual sua data de nascimento:", data_nascimento)
 print("Qual a cidade que reside no momento: ", local_onde_reside)
-print("Qual é a sua idade:", soma_idade_wellington) """
+print("Qual é a sua idade:", soma_idade_wellington)"""
 
 # Ser descritivo nos nomes das variaveis como por ex, eu quero criar uma variavel para saber
 # o nome de uma cidade, terei que colocar ex: nome_cidade e não de uma forma abreviada
@@ -21,10 +21,17 @@ print("Qual é a sua idade:", soma_idade_wellington) """
 
 "Ctrl + Shift + A = transformar um comando em comentario"
 
-nome = "Wellington"
-idade = 29
-maior_idade = idade >= 18
-print("nome", nome, "Idade", idade)
-print("É maior de idade:", maior_idade)
+# nome = "Wellington"
+# idade = 29
+# maior_idade = idade >= 18
+# print("nome", nome, "Idade", idade)
+# print("É maior de idade:", maior_idade)
+
+name = "Wellington"
+age = 15
+menor_de_idade = age <= 18
+print("nome:", name)
+print("Age:", age)
+print("É menor de idade:", menor_de_idade)
 
 
