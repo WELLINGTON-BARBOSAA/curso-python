2b06e36a7f85601aa60d11ab884cc62fcfb14e36
Wellington <wgn1910ww@gmail.com>
2024-05-07 14:44:52 -0300

    Revisão aula11

--- arquivos/aula11.py
+++ arquivos/aula11.py
@@ -1,8 +1,12 @@
 # Ordem dos operadores 
-# 1. (n + n) -> na programação é dado prioridade aos parenteses ()
+# 1. (n + n) -> na programação é dado prioridade aos parenteses () é feito de dentro para fora
 # 2. ** exponenciação 
 # 3. */ // %
 # 4. + -
+
+conta1 = (1 + 1) ** (5 + 5)         #conta1 = 1 + 1 ** 5 + 5 Resultado deu 7 por causa da ordem
+# dos operadores
+print(conta1)
 
 conta_1 = (5 + 5) // (1 + 1) 
 conta_2 = (1 + 1) ** (5 + 5) 
