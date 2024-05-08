2acf7feff6ff7638f2b0bc1534093217d7942d23
Wellington <wgn1910ww@gmail.com>
2024-05-07 21:26:40 -0300

    Atv de IMC

--- arquivos/aula12.py
+++ arquivos/aula12.py
@@ -1,10 +1,11 @@
-nome = "Wellington Gomes"
+nome = "Wellington"
 altura = 1.70
-peso = 78 
+peso = 71
 imc = peso / (altura*altura)
 
-print("Nome do paciente:", nome)
-print("Altura do paciente:", altura)
-print("Seu peso atual:", peso)
-print("Fazendo os devidos calculos, seu imc é de:", imc)
-print("Então o paciente se encontra em Sobrepeso")
+print("Paciente", nome)
+print("Altura", altura)
+print("Peso", peso)
+print("Seu imc é de:", imc)
+print("Depois de anos seu peso esta normal")
+print("Parabéns")
