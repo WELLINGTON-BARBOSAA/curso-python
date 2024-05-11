b75904590179565e49e1c8502621084306b7c1ac
Wellington <wgn1910ww@gmail.com>
2024-05-11 11:47:13 -0300

    Revisando aula if,elif e else. Part2

--- arquivos/aula17.py
+++ arquivos/aula17.py
@@ -1,28 +1,35 @@
 # if / elif     /else
 # se / se não se/ se não
 """No Boolean, só será executado o que for verdadeiro"""
+
 condição1 = False
 condição2 = False
 condição3 = False
-condição4 = False
+condição4 = True
 condição5 = False
+condição6 = False
+condição7 = False
 
 if condição1:
-    print("Condição 1 é falsa")
+    print("Essa condição não será executada")
 
 elif condição2:
-    print("Condição 2 é falsa")
+    print("Essa condinção não será executada")
 
-elif condição3:
-    print("Condição 3 é falsa")
+elif condição3: 
+    print("Essa condição não sera executada")
 
 elif condição4:
-    print("Condição 4 é falsa")
+    print("Minha condição foi executada")
 
 elif condição5:
-    print("Esta condição é Verdadeira")
+    print("Não vai ser executada")
+
+elif condição6:
+    print("Não vai ser executada")
+
+elif condição7:
+    print("Essa condição não será executada")
 
 else:
-    print("nenhuma das condições foram executadas")
-    print("Sair dos sistema")
-    
+    print("Nenhuma das condições foi executada")
