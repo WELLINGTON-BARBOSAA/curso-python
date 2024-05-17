6d10a3d7349ed1cf2ea31be94c2187d1caa9f1fd
Wellington <wgn1910ww@gmail.com>
2024-05-17 19:37:23 -0300

    Aula sobre not

---
+++ arquivos/aula23.py
@@ -0,0 +1,16 @@
+# Operador lógico "not"
+# Usado para inverter expressões
+# not True = False
+# not False = True
+
+
+# comida = input("Qual é a melhor comida do mundo: ")
+
+# if not comida == "Brigadeiro":
+#     print("você acertou")
+# else:
+#     print('Voce errou')
+
+senha = input ("Digite a senha")
+senha == "123456"
+print = senha
