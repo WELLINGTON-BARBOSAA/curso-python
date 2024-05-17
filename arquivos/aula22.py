285c8f2f031493019cf2d74d2532e8d4ae6b46e6
Wellington <wgn1910ww@gmail.com>
2024-05-17 18:34:57 -0300

    Aula sobre or

---
+++ arquivos/aula22.py
@@ -0,0 +1,28 @@
+# Operadores lógicos
+# and (e) or (ou) not (não)
+# and - todas as condições precisam ser verdadeiras
+# Se qualquer valor for considera falso
+# a expressão inteira será avaliada naquele Valor 
+# São considedaros falsy (que eu ja vi)
+# 0 0.0 '' False
+# Também existe o tipo None que é
+# usado para respresentar um não valor
+
+# ESTA AULA FALA SOBRE OR
+
+entrada = input("Para entrar digite [E] ou [S] para sair do sistema: ")
+senha_usuario = input ("Digite sua senha: ")
+senha_permitida = "7411"
+
+if entrada == 'E' and entrada == 'e' or senha_usuario == senha_permitida:
+    print("voce entrou no sistema")
+else:
+    print("saiu do sistema")
+
+# senha = input ("senha: ") or 'não digitou a senha'
+# print(senha)
+
+
+# # print(False or False or False or 'abc')
+
+# #Acontece porque no or ele pula o os False até encontrar True
