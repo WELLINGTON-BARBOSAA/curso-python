63138a4fe89e80e9e4da8e525fe655489c4bf140
Wellington <wgn1910ww@gmail.com>
2024-05-21 19:41:36 -0300

    Aula fatiamento de str

---
+++ arquivos/aula27.py
@@ -0,0 +1,23 @@
+'''
+Fatiamento de strings
+ 012345678
+ olá mundo
+-987654321
+Fatiamento [i:f:p] [::]
+Obs.: a função len retorna a qtd
+de caracteres str
+''' 
+variavel = "Olá mundo"
+print(variavel[0:7:2])
+# Sobre onde começar e terminar [4:7:2] = 0 indica de onde vai começar, 7 a quantidade de str 
+# que existe e 2 a quantidade de pulos que vai acontecer
+# 
+
+
+
+
+# nome = "Wellington Gomes de Sousa Barbosa"
+# resultado = len(nome)
+# print(f"O tamanho em str do seu nome completo é {resultado}")
+# Obs.: a função len retonra a qtd
+# de caracteres str
