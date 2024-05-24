583396459d4ff98cc1fb4c5e587041e0b51bf5fc
Wellington <wgn1910ww@gmail.com>
2024-05-23 21:22:05 -0300

    Atividade resolvida

--- arquivos/aula28atv.py
+++ arquivos/aula28atv.py
@@ -10,24 +10,47 @@
 #         A primeira letra do seu nome é {letra}
 #         A ultima letra do seu nome é {letra}
 # Se nada for digitado em nome ou idade:
-#     Exiba "Desculpe, você deixou campos vazios.
+# #     Exiba "Desculpe, você deixou campos vazios.
 
-nome = input ("Digite seu nome: ")
-idade = input ("Digite sua idade: ")
+# nome = input(f'Digite seu nome: ')
+# idade = input(f'Sua idade é: ')
+
+# if nome and idade:
+#     print(f"Seu nome é {nome}")
+#     print(f'Seu nome invertido é {nome[::-1]}')
+
+#     if ' ' in nome:
+#         print("Seu nome tem espaço")
+
+#     else:
+#         print("Seu nome NÃO tem espaço")
+    
+#     print(f'Seu nome tem {len(nome)[0]} letras')
+#     print(f'A primeira letra do seu nome é {nome[0]}: ')
+#     print(f'A ultima letra do seu nome é {nome[-1]}')
 
 
-if nome:
-    print(f'Seu nome é : {nome}')
-    print(f'Seu nome é invertido é : {nome[::-1]}')
-    print(f'A primeira letra do seu nome é : {nome[0]}')
-    print(f'A ultima letra do seu nome é : {nome[9]}')
-    print(len(nome))
+# else:
+#     print("Voce deixou campos vazios") 
+
+# EXERCICIO RESOLVIDO PELO LUIZ OTÁVIO 
+
+nome =input("Digite seu nome: ")
+idade =input("Digite sua idade: ")
+
+if nome and idade:
+    print(f'Seu nome é {nome}')
+    print(f'Sua idade é {idade}')
+    print(f'Seu nome invertido é {nome[::-1]}')
+
+    if ' ' in nome:
+        print("Seu nome TEM ESPAÇOS")
+    else:
+        print('Seu nome NÃO tem espaço')
+    
+    print(f"Seu nome tem {len(nome)} letras")
+    print(f"A primeira letra do seu nome é: {nome[0]} ")
+    print(f"A ultima letra do seu nome é: {nome[-1]}")
+
 else:
-    print("Desculpe, voce deixou campos vazios")
-
-# Faltando como é contar se tem ou não espaço no nome
-
-
-
-
-
+    print('Voce deixou campos vazios')
