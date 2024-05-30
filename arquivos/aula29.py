96be15d4c0968fb9a2553e1efda13270d5a0eeb2
Wellington <wgn1910ww@gmail.com>
2024-05-30 18:42:52 -0300

    Aula try e exceptOK

--- arquivos/aula29.py
+++ arquivos/aula29.py
@@ -3,24 +3,23 @@ Introdução ao try/except
 try -> tentar executar o codigo
 except -> ocorreu algum erro ao tentar executar'''
 
-numero_str = input('Vou dobrar o numero que vc digitou: ')
-# DICA - > Para mudar o nome de uma variavel sem precisar
-# mudar todas, clica na  primeira variavel e aperte F2
-try:
-    numero_float = float(numero_str)
-    print("FLOAT: ", numero_float)
-    print(f'O dobro de {numero_str} é {numero_float * 2 :.2f}')
-except:
-    print('Isso não é um número')
-
-
-# numero_1 = input  ("Digite o primeiro numero: ")
-# numero_2 = input  ("Digite o segundo numero: ")
-# conta_1 = int (numero_1)
-# conta_2 = int (numero_2)
-
+# numero_str = input('Vou dobrar o numero que vc digitou: ')
+# # DICA - > Para mudar o nome de uma variavel sem precisar
+# # mudar todas, clica na  primeira variavel e aperte F2
 # try:
-#     print(f"Resultado da conta é: {conta_1 + conta_2}")
-
+#     numero_float = float(numero_str)
+#     print("FLOAT: ", numero_float)
+#     print(f'O dobro de {numero_str} é {numero_float * 2 :.2f}')
 # except:
-#     print('Voce não digitou um numero')
+#     print('Isso não é um número')
+
+numero_1 = input('Digite o primeiro numero: ')
+numero_2 = input('Digite o segundo numero: ')
+
+try:
+    conta_1 = float(numero_1)
+    conta_2 = float(numero_2)
+    print(f'O resultado da conta é: {conta_1 + conta_2:.1f}')
+except:
+    print('Voce não digitou numero')
+    print('Tente novamente')
