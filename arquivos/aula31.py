40a8f1416ae46d43f6ae7754151e17cbfab06eee
Wellington <wgn1910ww@gmail.com>
2024-06-08 18:19:06 -0300

    Aula31 sobre Flag, None, is e is not, e id

---
+++ arquivos/aula31.py
@@ -0,0 +1,19 @@
+'''
+Flag (Bandeira) - Marcar um local
+None = não valor
+is e is not = é ou não (Tipo, valor, identidade)
+id = Identidade
+'''
+v1 = 'a'
+v2 = 'c'
+
+print(id(v1))
+print(id(v2))
+
+# condição = False
+# if condição:
+#   print('Faça algo')
+# else?
+#   print ('Não faça algo')
+
+# E como o Python busca a identidade do objeto na memória, id.
