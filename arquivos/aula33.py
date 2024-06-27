08801302df2f7bdf77a3895e64bd47611c826d69
Wellington <wgn1910ww@gmail.com>
2024-06-27 19:10:54 -0300

    Aula33 - objetos imutaveis

---
+++ arquivos/aula33.py
@@ -0,0 +1,17 @@
+'''
+https: //docs.pytnhon.org/pt-br/3/library/stdtypes.html
+imutaveis que vimos: str, int, float, bool'''
+# str, int, float, bool = imutaveis, significa que não pode mudar seu valor
+
+
+string = 'wellington'
+outra_variavel = f'{string[4:]}ABC{string[:4]}'
+print(string)
+print(outra_variavel)
+print(string.capitalize())
+# exemplo de como tentar mudar um obejto imutavel, utilizando uma nova variavel 
+# e editando de acordo com sua necessidade
+
+#Fatiamento = [4:0]de onde eu quero que comece
+#Fatiamento = [:4]de onde eu quero que termine
+
