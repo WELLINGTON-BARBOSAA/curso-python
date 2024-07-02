141a298cbb8bcaff8fba1dd21191bd94cc643bb4
Wellington <wgn1910ww@gmail.com>
2024-07-02 19:47:57 -0300

    Aula36 - conhecendi mais operadores de atribuição

---
+++ arquivos/aula36.py
@@ -0,0 +1,13 @@
+''' Operadores de atribuição
+= += -= *= /= //= **= %='''
+
+"""Exercico para fortalecer a mente. Eu quero um programa que conte até mil só que
+pulando de 10 em 10 """
+
+contador = 0
+
+while contador <= 1000:
+    contador += 10
+    print(contador)
+
+# A cada aprendizado eu preciso fazer exercicios para fortalecer a mente 
