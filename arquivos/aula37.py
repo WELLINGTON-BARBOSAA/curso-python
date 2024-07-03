99e04929cd2052630c8af91732aa0644766b7947
Wellington <wgn1910ww@gmail.com>
2024-07-03 14:43:35 -0300

    While and continue com exercicios

---
+++ arquivos/aula37.py
@@ -0,0 +1,28 @@
+"""
+Repetições
+While (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando o codigo não tem fim """
+
+
+"""Atividade minha para fortalecer a mente
+Escreva um codigo que conte até 100 OK
+Depois faça o programa quebrar com 70 fazendo ele pular do 30 a 50 sem contar"""
+
+contador = 0 
+
+while contador <= 100:
+    contador += 1
+    if contador == 5:
+        print('não quero mostrar o', contador)
+        continue
+    if contador >= 10 and contador <= 30:
+        print('Não quero mostrar o', contador)
+        continue
+
+    print(contador)
+
+    if contador == 80:
+        break
+    
+print("Acabou")
--- arquivos/tempCodeRunnerFile.py
+++ arquivos/tempCodeRunnerFile.py
@@ -1,5 +1,1 @@
-# nome = "Wellington Gomes de Sousa Barbosa"
-# resultado = len(nome)
-# print(f"O tamanho em str do seu nome completo é {resultado}")
-# Obs.: a função len retonra a qtd
-# de caracteres str
+print("Não quero que mostre o 3")
