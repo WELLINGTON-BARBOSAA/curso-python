f7b5796ae12792230a6181477c6c44940636f989
Wellington <wgn1910ww@gmail.com>
2024-07-06 10:47:09 -0300

    Str interando com while

---
+++ arquivos/aula39.py
@@ -0,0 +1,21 @@
+"""
+Iterando strings com while
+"""
+#       012345678910
+# nome ='Luiz Otávio' # Iteráveis
+#      1110987654321
+
+
+nome = 'Welllington'  # Iteráveis
+
+indice = 0
+novo_nome = ''
+while indice < len(nome):
+    letra = nome [indice]
+    novo_nome += f'*{letra}'
+    indice += 1
+
+print(novo_nome)
+
+    
+    
--- arquivos/tempCodeRunnerFile.py
+++ arquivos/tempCodeRunnerFile.py
@@ -1,1 +1,9 @@
-print("Não quero que mostre o 3")
+
+indice = 0
+novo_nome = ''
+while indice < len(nome):
+    letra = nome [indice]
+    novo_nome += f'/{nome}'
+    indice += 1
+
+print(novo_nome)
