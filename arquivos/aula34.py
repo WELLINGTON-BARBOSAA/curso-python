8c3d44b29a8880f94c5f32aabaaa3d3af631e233
Wellington <wgn1910ww@gmail.com>
2024-07-18 21:06:19 -0300

    Revisando while porque tive duvidas

--- arquivos/aula34.py
+++ arquivos/aula34.py
@@ -6,12 +6,10 @@ Executa uma ação enquanto a condição for verdadeira'''
 condicao = True
 
 while condicao:
-    nome = input('Digite seu nome completo: ')
-    print(f'Seu nome completo é: {nome}')
-    
-    if nome == "Sair":
-        break
-print('Voce saiu do programa')
+    nome = input('Digite seu nome: ')
+    print(f'Seu nome é {nome}')
 
+    if nome == 'Sair':
+        break
 
 # Crtl + C é um comando que interrompe o programa quando aparece em loop infinito
