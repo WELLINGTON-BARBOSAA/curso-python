86a84c348aba9802a5fc4d80006a832df577a8ff
Wellington <wgn1910ww@gmail.com>
2024-07-24 20:45:44 -0300

    Aula sobre for e range

---
+++ arquivos/aula44.py
@@ -0,0 +1,13 @@
+'''
+For + Range
+range -> range(start, stop, step)'''
+
+# numeros = range(10)
+# numeros = range(5, 10)
+# numeros = range(5, 10, 2)
+# print(numeros) 
+
+numeros = range(1, 15, 3)
+
+for numero in numeros:
+    print(numero)
