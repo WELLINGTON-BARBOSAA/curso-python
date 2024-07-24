0bba90c791baddb29bce54b9bdf65c36a2854750
Wellington <wgn1910ww@gmail.com>
2024-07-24 20:39:06 -0300

    Arquivo treino

--- arquivos/arqtreino.py
+++ arquivos/arqtreino.py
@@ -1,8 +1,22 @@
-contador = 0 
+nome = 'Eduarda'
+i = 0
+letra_por_letra = ''
 
-while contador <= 10:
-    contador += 1
-    if contador >= 5 and contador <= 8:
-        print(f'Não quero mostrar o numero {contador}')
-        continue
-    print(contador)
+while i < len(nome):
+    letra_por_letra = nome [i]
+    letra_por_letra += f'*{letra_por_letra}*'
+    i += 1
+
+    print(letra_por_letra)
+
+
+# nome = 'Welllington'  # Iteráveis
+
+# indice = 0
+# novo_nome = ''
+# while indice < len(nome):
+#     letra = nome [indice]
+#     novo_nome += f'*{letra}'
+#     indice += 1
+
+# print(novo_nome)
