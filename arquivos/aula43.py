bf010f89d022ec5ee50cf51e8626faad8df659a1
Wellington <wgn1910ww@gmail.com>
2024-07-23 21:35:12 -0300

    Introdução ao for

--- arquivos/aula43.py
+++ arquivos/aula43.py
@@ -1,11 +1,21 @@
 ' Aula sobre laço de repetição - for'
 
+# senha_salva = '123456'
+# senha_digitada = ''
+# repeticoes = 0 
 
-nome = 'Wellington Gomes de Sousa Barbosa'  
-'tenho 29 anos e meu sonho é ser programador'
+# while senha_salva != senha_digitada:
+#         senha_digitada = input(f'Sua senha ({repeticoes}x): ')
 
+#         repeticoes += 1
+# print(f'Seu laço teve *{repeticoes}* repetições')
+# print('O laço a seguir pode ter repetições infinitas')
+
+nome = 'Bala'
+novo_nome = ''
 for letra in nome:
+    novo_nome += f'/{letra}'
     print(letra)
-
+print(novo_nome)
 
 
