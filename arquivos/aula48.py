8e05d33ab587628bdad9565fcfa53abf3534d8e4
Wellington <wgn1910ww@gmail.com>
2024-08-03 19:37:47 -0300

    Introdução a Listas

---
+++ arquivos/aula48.py
@@ -0,0 +1,21 @@
+"""
+
+
+Listas em Python
+Tipo list - Mutável 
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - indices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +"""
+
+#........+01234
+#........-54321
+string = 'ABCDE' # 5 caracteres (len)
+
+#........0.....1........2..........3...4 
+#.......-5...-4.......-3.........-2...-1 
+lista = [123, True, "Wellington", 1.2, []]
+lista[-3] = 'Maria' # Nessa linha é alterado a str de 'Wellington' para 'Maria'
+print(lista)
+print(lista[2], type(lista[2]))
+
+
