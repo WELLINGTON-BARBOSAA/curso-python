599886ce1c3056e3ed1e50d26ab31871ce7a0fe6
Wellington <wgn1910ww@gmail.com>
2024-08-06 20:22:02 -0300

    praticando listas

--- arquivos/aula48.py
+++ arquivos/aula48.py
@@ -13,9 +13,16 @@ string = 'ABCDE' # 5 caracteres (len)
 
 #........0.....1........2..........3...4 
 #.......-5...-4.......-3.........-2...-1 
-lista = [123, True, "Wellington", 1.2, []]
-lista[-3] = 'Maria' # Nessa linha é alterado a str de 'Wellington' para 'Maria'
+# lista = [123, True, "Wellington", 1.2, []]
+# lista[-3] = 'Maria' # Nessa linha é alterado a str de 'Wellington' para 'Maria'
+# print(lista)
+# print(lista[2], type(lista[2]))
+
+lista = [1, False, 55, 3.2, 'Wellington' , 100] # conchetes significa esta chamando a lista []
+lista[4] = 'Maria' # Aqui eu mudei o valor de 'Wellington' para 'Maria'
 print(lista)
-print(lista[2], type(lista[2]))
+print(lista[0], lista[3], lista[5])
+
+
 
 
