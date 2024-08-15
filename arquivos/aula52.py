01a35d5b62b0c3acbd136adcf608c7ccab19404c
Wellington <wgn1910ww@gmail.com>
2024-08-14 21:46:09 -0300

    introduação a tuplas

---
+++ arquivos/aula52.py
@@ -0,0 +1,11 @@
+# Tipo tupla - Uma lista mutavel
+
+nomes = 'Wellington', 'Vai ser', 'Programador' # Tuplas são listas imutaveis
+nomes = tuple(nomes) 
+nomes = list(nomes)
+print(nomes)
+print(nomes[1])
+
+
+# tuplas são representadas por ()
+# listas são representadas por []
