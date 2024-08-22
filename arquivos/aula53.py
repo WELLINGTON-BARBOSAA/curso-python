0131b827fa4192e21a03747639f95d4bfd3c70f4
Wellington <wgn1910ww@gmail.com>
2024-08-21 21:19:50 -0300

    Enumerate - enumera iteraveis

---
+++ arquivos/aula53.py
@@ -0,0 +1,30 @@
+'Enumerate - enumera os iteraveis (indices)'
+
+lista = ['Wellington', 'Vai ser', 'Programador']
+
+# for item in enumerate(lista):
+#     indice, nome = item
+#     print(indice, nome)
+
+for tupla_enumerada in enumerate(lista):
+    print('For da tupla')
+    for valor in tupla_enumerada:
+        print(f'\t{valor}')
+
+
+
+
+
+
+
+# lista_enumerada = tuple(enumerate(lista))
+# lista_enumerada = list(enumerate(lista)) # coerção de lista e tupla, outro jeito de enum iteravel
+# lista_enumerada = tuple(enumerate(lista))
+
+# print(lista_enumerada)
+
+# for item in enumerate(lista): um metodo de fazer enumerar iterador
+#     print(item)
+
+# lista_enumerada=(enumerate(lista))
+# print(next(lista_enumerada))
