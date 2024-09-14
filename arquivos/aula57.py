f662f8601370c92ceab763a9e67c109f9bad04b5
Wellington <wgn1910ww@gmail.com>
2024-09-14 11:26:29 -0300

    Continuação da aula de lista dentro de listas

---
+++ arquivos/aula57.py
@@ -0,0 +1,17 @@
+salas = [
+
+['Antonio', 'Fernando', 'Pinheiro'], 
+
+['Jose', 'Miguel', 'Fernanda', 'Raimunda'],
+
+['Rafael', 'Jose', 'Augusto'],
+
+]
+# print(lista_alunos[0][1])
+# print(lista_alunos[1][1])
+# print(lista_alunos[2][2])
+
+for sala  in salas:
+    print(sala)
+    for aluno in salas:
+        print(aluno)
