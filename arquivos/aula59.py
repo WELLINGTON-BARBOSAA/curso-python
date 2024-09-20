a1f37d107d4ff4a4444c0f999bdd8f9fd8ef8f2d
Wellington <wgn1910ww@gmail.com>
2024-09-19 22:00:36 -0300

    Desempacotamento de metodos e funções

---
+++ arquivos/aula59.py
@@ -0,0 +1,24 @@
+# Desempacotamento em chamadas
+# de metodos e funções
+
+# string = 'ABC'
+# lista = ['Maria', 'Helena', 1, 2, 3, 'Eduarda']
+# tupla = 'Python', 'é', 'legal'
+
+# for nome in lista:
+#     print(nome, end=' ') # esse movimento quebra a linha 
+
+# print(*lista)
+# print(*string)
+# print(*tupla)
+
+residencia = [
+
+['Jose Alfredo', 'Raimundo Neto', 'Augusto Antonio'],
+['Fernada Lima', 'Monique Santanda', 'Bruno Cesar'],
+['Rihana Gomes', 'Lebron Barbosa', 'Stan lee'],
+
+]
+
+
+print(*residencia, sep='\n') # Esse comando desempacota e quebra linha deixando organizado o código
