a35c03dc9c3e147df8d7baf64607fee32b51d172
Wellington <wgn1910ww@gmail.com>
2024-10-23 20:54:58 -0300

    revisando aula 56 de split and join com list e str

--- arquivos/aula56.py
+++ arquivos/aula56.py
@@ -1,11 +1,13 @@
+# F3 abre um caixa de pesquisa para buscas de caracteres dentro do código
+
 """
 split e join com list e str
 split - divide uma string (retorna uma lista)
 join - une uma string"""
-# strip corta os espaços onde tiver\rtrip corta os da direita e lstrip corta os da esquerd
+# strip corta os espaços onde tiver\'rtrip' corta os da direita e 'lstrip' corta os da esquerd
 
 frase = 'Olha só, a Duda passou em medicina'
-lista_frases_cruas = frase.split(',') # divide um uma string em determinado caracter
+lista_frases_cruas = frase.split(',') # divide uma string em determinado caracter
 
 lista_frases = []
 for i, frase in enumerate(lista_frases_cruas):
@@ -17,7 +19,7 @@ for i, frase in enumerate(lista_frases_cruas):
 frases_unidas = '/'.join(lista_frases) # '/' indica qual caractere eu quero usar como separador
 print(frases_unidas)
 
-
 # join é um metodo de string, que cria ela vazia
 # vai ser usado para separação pa os itens do seu iteravel
-# apenas string, lista e tuplas. Se caso colocar numeros no metodo join o codigo da erro
+# apenas string, lista e tuplas. Se caso colocar numeros no metodo join o codigo da erro, porque
+# numeros não são iteráveis 
