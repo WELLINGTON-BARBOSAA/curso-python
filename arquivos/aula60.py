90d908b03bf041330939fce90d1d2fda566171cf
Wellington <wgn1910ww@gmail.com>
2024-10-29 21:59:53 -0300

    Introdução a condição ternaria - if and else na mesma linha

---
+++ arquivos/aula60.py
@@ -0,0 +1,24 @@
+'''
+Operação ternaria (condição de uma linha)
+<valor> if <condicao> else <outro valor>'''
+
+
+condição1 = 1 == 8
+condição2 = 3 == 5
+condição3 = 5 == 8
+condição4 = 3 == 3
+condição5 = 4 == 4 
+
+cond1 = 'VERDADEIRO' if condição1 else 'FALSO'
+cond2 = 'VERDADEIRO' if condição2 else 'FALSO'
+cond3 = 'VERDADEIRO' if condição3 else 'FALSO'
+cond4 = 'VERDADEIRO' if condição4 else 'FALSO'
+cond5 = 'VERDADEIRO' if condição5 else 'FALSO'
+
+print(cond1)
+print(cond2)
+print(cond3)
+print(cond4)
+print(cond5)
+
+
