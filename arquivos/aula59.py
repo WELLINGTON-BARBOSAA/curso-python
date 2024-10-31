ab9702283ea6ffab232cc33a4833cd548c2c0d8b
Wellington <wgn1910ww@gmail.com>
2024-10-31 20:38:42 -0300

    desempacotamento em chamdas de metods and funções

--- arquivos/aula59.py
+++ arquivos/aula59.py
@@ -1,24 +1,24 @@
-# Desempacotamento em chamadas
-# de metodos e funções
+#Desempacotamento em chamadas
+#de metodos e funções
 
-# string = 'ABC'
-# lista = ['Maria', 'Helena', 1, 2, 3, 'Eduarda']
-# tupla = 'Python', 'é', 'legal'
+string = 'ABC'
+lista = ['Maria', 'Helena', 1, 2, 3, 'Eduarda']
+tupla = 'Python', 'é', 'legal'
 
-# for nome in lista:
-#     print(nome, end=' ') # esse movimento quebra a linha 
+for nome in lista:
+    print(nome, end=' ') # esse movimento quebra a linha 
 
-# print(*lista)
-# print(*string)
-# print(*tupla)
+print(*lista)
+print(*string)
+print(*tupla)
 
-residencia = [
+# residencia = [
 
-['Jose Alfredo', 'Raimundo Neto', 'Augusto Antonio'],
-['Fernada Lima', 'Monique Santanda', 'Bruno Cesar'],
-['Rihana Gomes', 'Lebron Barbosa', 'Stan lee'],
+# ['Jose Alfredo', 'Raimundo Neto', 'Augusto Antonio'],
+# ['Fernada Lima', 'Monique Santanda', 'Bruno Cesar'],
+# ['Rihana Gomes', 'Lebron Barbosa', 'Stan lee'],
 
-]
+# ]
 
 
-print(*residencia, sep='\n') # Esse comando desempacota e quebra linha deixando organizado o código
+# print(*residencia, sep='\n') # Esse comando desempacota e quebra linha deixando organizado o código
