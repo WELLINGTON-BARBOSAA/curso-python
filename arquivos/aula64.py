a7ba2326c63979d8c8ff9306877038408105aaaf
Wellington <wgn1910ww@gmail.com>
2024-12-06 20:41:25 -0300

    introdução a função

---
+++ arquivos/aula64.py
@@ -0,0 +1,17 @@
+# def numero (a, b, c):
+#     print(a, b, c)
+
+# numero(1, 2, 3)
+# numero(4, 5, 6)
+
+
+
+def saudacao(nome):
+    print(f'Seja bem vindo {nome}')
+
+saudacao('Programador')
+saudacao('Wellington')
+saudacao('Gomes')
+saudacao('de')
+saudacao('Sousa')
+saudacao('Barbosa')
