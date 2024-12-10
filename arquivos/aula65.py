7f6f6c345983bd84bd8c0b2a313f3d48b67faa46
Wellington <wgn1910ww@gmail.com>
2024-12-09 21:33:49 -0300

    introdução a fnção

---
+++ arquivos/aula65.py
@@ -0,0 +1,27 @@
+"""
+Introdução as funções (def) em Python
+Funções são trechos de codicos usar para 
+replicar determinada ação ao longo do seu código.
+Elas podem receber valores para parametros (argumentos)
+e retornar um valor especifico. 
+Por padrão, funções Python retorna None (nada) """
+
+
+# def imprimir (a, b, c):
+#     print(a, b, c)
+
+# imprimir(1, 2, 3)
+# imprimir(4, 5, 6)
+# imprimir(7, 8, 9)
+# imprimir(10, 11, 12)
+# imprimir(13, 14, 15)
+# imprimir(16, 17, 18)
+# imprimir(19, 20, 21)
+
+def bem_vindo (nome):
+    print(f'Seja bem- vindo {nome}')
+
+bem_vindo('Wellington')
+bem_vindo('Fernanda')
+bem_vindo('Maria')
+bem_vindo('Antonia')
