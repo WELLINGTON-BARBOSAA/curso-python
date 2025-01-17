efb5f30085453c328e13afb456f259930ac41d8d
Wellington <wgn1910ww@gmail.com>
2025-01-16 22:22:27 -0300

    Atv3 - soma de 2 numeros

---
+++ atividades/atv3.py
@@ -0,0 +1,11 @@
+'Faça um Programa que peça dois números e imprima a soma.'
+
+numero1=input('Digite o primeiro numero: ')
+numero2=input('Digite o segundo numero: ')
+
+num1=int(numero1)
+num2=int(numero2)
+
+soma= num1 + num2
+
+print(f'O resultado da soma é: {soma}')
