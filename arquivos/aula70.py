13fb07b578dea68b6a6978b56be01fd627655fda
Wellington <wgn1910ww@gmail.com>
2025-01-18 19:18:51 -0300

    Introdução a return

---
+++ arquivos/aula70.py
@@ -0,0 +1,26 @@
+'''
+Retorno de funções
+'''
+
+
+# def soma (x,y,z):
+
+#     return x + y + z
+# soma1 = soma(4,5,5)
+# soma2 = soma(3,6,8)
+# print(soma1 + soma2)
+
+
+# def soma (x,y):
+#     if x > 10:
+#         return 10,20
+#     return x + y
+
+# soma1 = soma(2,2)
+# soma2 = soma(3,3)
+# print(soma(11,55))
+
+def quadrado (num):
+    return num * num
+
+print(quadrado(5))
