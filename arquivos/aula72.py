83e70dd829ac80bc871883ae38191df0bb280fde
Wellington <wgn1910ww@gmail.com>
2025-01-25 21:02:12 -0300

    revisando atv - multiplicador e par_impar

--- arquivos/aula72.py
+++ arquivos/aula72.py
@@ -1,34 +1,24 @@
-# Exercicios com funções
 
-# Crie uma função que multiplica todos os argumentos
-# não nomeados recebidos
-# Retorne o total para uma variavel e mostre o valor
-# da variavel
 
-# def multiplicacao (*args):
+# def multiplicador (*args):
 #     total = 1
-#     for numeros in args:
-#         total *= numeros
-#     return total # para o return ser executado corretamente, sempre colocar um tab a frente
-#                  # da função  
-# resultado = multiplicacao(1,2,3,4,5,6)
-# print(resultado)
-
-# Crie uma função que fala se o numero é par ou ímpar.
-# Retorne se o numero é par ou ímpar
+#     for numero in args:
+#         total *= numero    
+#     return total
 
 
-# def par_impar(x):
-#     return x % 2 == 0
 
-# resultado = par_impar(2)
-# print(resultado)
+# resultado = multiplicador(1 , 2, 3, 4, 5, 6, 7, 8, 9)
+# print(f'O resultado da conta é : {resultado}')
+
+
 
 def par_impar (numero):
     multiplo_de2 = numero % 2 == 0
     if multiplo_de2:
-        return 'f {numero} é par' 
-    return 'f {numero} é impar'
+        return f'{numero} é par' 
+    return f'{numero} é impar'
+
+print(par_impar(8))
 
 
-print(par_impar(5))
