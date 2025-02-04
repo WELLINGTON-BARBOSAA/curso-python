74014b1232b363a7b46ce01c35669ab67edfeed9
Wellington <wgn1910ww@gmail.com>
2025-02-04 18:39:24 -0300

    Atividade de soma com funcao

---
+++ atividades/atv9.py
@@ -0,0 +1,8 @@
+def soma (a,b):
+    return a + b
+
+num1 = int(input('Digite um numero: '))
+num2 = int(input('Digite outro numero: '))
+
+resultado = soma(num1,num2)
+print(f'O resultado da soma é: {resultado}')
