560bc27911b6fb64413823c690d6a301e6851a7b
Wellington <wgn1910ww@gmail.com>
2025-02-05 21:34:31 -0300

    Correção da atividade funcao

--- arquivos/aula75.py
+++ arquivos/aula75.py
@@ -2,24 +2,24 @@
 # Crie funções que duplicam, triplicam e quadruplicam
 # o número recebido como parâmetro.
 
+# def criar_multiplicador(multiplicador):
+#     def multiplicar(numero):
+#         return numero * multiplicador
+#     return multiplicar
+
+
+# duplicar = criar_multiplicador(2)
+# triplicar = criar_multiplicador(3)
+# quadruplicar = criar_multiplicador(4)
+
+# print(duplicar(2))
+# print(triplicar(2))
+# print(quadruplicar(2)) 
+
 def criar_multiplicador(multiplicador):
-    def multiplicar(numero):
+    def multiplicar (numero):
         return numero * multiplicador
     return multiplicar
-
-
-duplicar = criar_multiplicador(2)
-triplicar = criar_multiplicador(3)
-quadruplicar = criar_multiplicador(4)
-
-print(duplicar(2))
-print(triplicar(2))
-print(quadruplicar(2))
-
-def criar_multiplicador(multiplicador):
-    def multiplicador(numero):
-        return numero * multiplicador
-    return multiplicador
 
 multiplicar = criar_multiplicador(2)
 triplicar = criar_multiplicador(3)
