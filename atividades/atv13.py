4200452026daedfc45a7b489652cd9cca314c8e3
Wellington <wgn1910ww@gmail.com>
2025-02-14 18:40:25 -0300

    atv - Calculando area de um circulo

---
+++ atividades/atv13.py
@@ -0,0 +1,10 @@
+# Faça um Programa que peça o raio de um círculo, calcule e mostre sua área. 
+
+# C = 2 · 3,1 · 6
+
+raio_de_circulo = float (input('Digite um numero: '))
+
+calculo1 = raio_de_circulo / 2
+
+formula = 2.0 * 3.1 * calculo1
+print(f'Você digitou {raio_de_circulo} e sua área total é de {formula} cm')
