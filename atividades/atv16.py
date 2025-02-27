0c31f501188f6f7a4dae46037892fb3033e506c9
Wellington <wgn1910ww@gmail.com>
2025-02-27 19:47:20 -0300

    Calculando a base de um triangulo

---
+++ atividades/atv16.py
@@ -0,0 +1,13 @@
+# Escreva um programa para ler as dimensões de um retângulo (base e altura),
+# calcule e apresente a área.
+
+base_triangulo = float(input('Digite o tamanho da base: '))
+altura_triangulo = float(input('Digite o tamanho da altura: '))
+
+area_calculada = (base_triangulo * altura_triangulo) /2
+
+print(f'Base do triangulo é de :{base_triangulo}')
+print()
+print(f'Altura do triangulo é de :{altura_triangulo}')
+print()
+print(f'E o valor da sua area é de : {area_calculada}cm')
