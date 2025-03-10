440ae7c5bad1f9de7e87f6afcf1aa1809de6a15e
Wellington <wgn1910ww@gmail.com>
2025-03-09 21:44:20 -0300

    Tabuada

---
+++ atividades/atv19.py
@@ -0,0 +1,16 @@
+# Tabuada por completo
+
+def tabuada (numero):
+    return numero * 9 # Para mudar a tabuada é só trocar o número dessa linha e executar. 
+
+print(tabuada(1))
+print(tabuada(2))
+print(tabuada(3))
+print(tabuada(4))
+print(tabuada(5))
+print(tabuada(6))
+print(tabuada(7))
+print(tabuada(8))
+print(tabuada(9))
+print(tabuada(10))
+
