7f4aac63c599f01fce0f9ceeb04228b872e2f7bc
Wellington <wgn1910ww@gmail.com>
2025-03-13 19:44:24 -0300

    atividade perguntas

--- arquivos/aula77.py
+++ arquivos/aula77.py
@@ -1,17 +1,56 @@
-# Manipulando chaves e valores em dicionarios
+# Exercício - sistema de perguntas e respostas
 
-pessoas = {}
+perguntas = [
+    {
+        'Pergunta': 'Quanto é 2+2?',
+        'Opções': ['1', '3', '4', '5'],
+        'Resposta': '4',
+    },
+    {
+        'Pergunta': 'Quanto é 5*5?',
+        'Opções': ['25', '55', '10', '51'],
+        'Resposta': '25',
+    },
+    {
+        'Pergunta': 'Quanto é 10/2?',
+        'Opções': ['4', '5', '2', '1'],
+        'Resposta': '5',
+    },
+]
+
+qtd_acertos = 0
+for pergunta in perguntas:
+    print('Pergunta:', pergunta['Pergunta'])
+    print()
+
+    opcoes = pergunta['Opções']
+    for i, opcao in enumerate(opcoes):
+        print(f'{i})', opcao)
+    print()
+
+    escolha = input('Escolha uma opção: ')
+
+    acertou = False
+    escolha_int = None
+    qtd_opcoes = len(opcoes)
+
+    if escolha.isdigit():
+        escolha_int = int(escolha)
+
+    if escolha_int is not None:
+        if escolha_int >= 0 and escolha_int < qtd_opcoes:
+            if opcoes[escolha_int] == pergunta['Resposta']:
+                acertou = True
+
+    print()
+    if acertou:
+        qtd_acertos += 1
+        print('Acertou 👍')
+    else:
+        print('Errou ❌')
+
+    print()
 
 
-chave = 'nome completo'
-
-pessoas[chave] = 'Luiz Otavio' # Criando Chaves
-pessoas['Sobrenome'] = 'Miranda'
-
-pessoas[chave] = 'Maria' #Editando chave
-#print(pessoas[chave])
-print(pessoas['Sobrenome'])
-
-del pessoas ['Sobrenome'] # Apagando chaves
-print(pessoas['Sobrenome'])
-
+print('Você acertou', qtd_acertos)
+print('de', len(perguntas), 'perguntas.')
