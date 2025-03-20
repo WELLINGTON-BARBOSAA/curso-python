f2db7c6f5c50a0d09081b00fd18a4b1ae8a22c79
Wellington <wgn1910ww@gmail.com>
2025-03-20 20:12:46 -0300

    atividade simples sobre set

---
+++ atividades/atv21.py
@@ -0,0 +1,10 @@
+# Atividade sobre set, removendo valores duplicados
+
+
+# lista1 = {0,0,0,0,1,1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,9,9,9,10,10,10}
+# print(lista1)
+
+
+lista2 = [51,51,52,52,53,53,54,54,55,55,56,56]
+lista22 = set(lista2)
+print(lista22)
