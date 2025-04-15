048ac99322cc131865eff7857b7aa5c10466cd52
Wellington <wgn1910ww@gmail.com>
2025-04-14 21:18:01 -0300

    Atividade de inserção de dados

--- atv_laç_repetição/atv3.py
+++ atv_laç_repetição/atv3.py
@@ -1,42 +1,68 @@
-'''    3. Faça um programa que leia e valide as seguintes informações: 
-        a. Nome: maior que 3 caracteres; 
-        b. Idade: entre 0 e 150; 
-        c. Salário: maior que zero; 
-        d. Sexo: 'f' ou 'm'; 
-        e. Estado Civil: 's', 'c', 'v', 'd'; 
-       Use a função len(string) para saber o tamanho de um texto (número de caracteres).'''
+''' 3. Faça um programa que leia e valide as seguintes informações: 
+a. Nome: maior que 3 caracteres; 
+b. Idade: entre 0 e 150; 
+c. Salário: maior que zero; 
+d. Sexo: 'f' ou 'm'; 
+e. Estado Civil: 's', 'c', 'v', 'd'; 
+Use a função len(string) para saber o tamanho de um texto (número de caracteres).'''
 
-nome = input('Digite sua nome: ')
-idade = int(input('Digite sua idade:'))
-salario = float(input('Digite seu salário: '))
-sexo = input('Digite o sexo(f ou m): ')
-est_civ = input('Digite estado civil (s,c,v ou d): ')
+def nome_valido (nome):
+    if not len(nome) > 3:
+        return # return vazio é = None
+    return nome   
 
-if len (nome) >=  3:
-    print('Numero validado')
+def idade_valida(idade):
+    if idade > 150:
+        return
+    return idade
 
-else: 
-    print('Numero invalido, tente novamente: ')
+def salario_valido(salario):
+    if not salario > 0:
+        return 
+    return salario
 
-if idade > 0 and idade <= 150:
-    print('Idade valida')
+def sexo_valido (sexo):
+    if not sexo in ('m', 'f'):
+        return 
+    return sexo
 
-else:
-    print('Idade invalida')
+def estado_valido(civil):
+    if not civil in ('s', 'c', 'd', 'v'):
+        return
+    return civil
 
-if salario > 0 :
-    print('Salario valido')
+while True:
 
-else:
-    print('Salario  invalido')
+    nome = input('Digite seu nome: ')
+    if nome_valido(nome): # if automaticamente checa se é verdadeiro. 
+        print('Nome valido')
+    else:
+        print('Nome invalido')
 
-if sexo in ('m', 'M', 's', 'S'): 
-    print('Sexo validado com sucesso')
-else:
-    print('Sexo negado, ex(s ou m)')
- 
-if est_civ in ('s', 'c', 'v', 'd'): 
-    print('Estado civil valido')
+    idade = int(input('Digite sua idade: '))
+    if idade_valida(idade):
+        print('Idade valida')
+    else:
+        print('Idade incorreta, tente novamente')
 
-else:
-    print('Estado civil invalido (ex: s, c, v ou d)')
+
+    salario = int(input('Digite seu salario: ') )
+    if salario_valido (salario):
+        print('Salario valido')
+    else:
+        print('Salario invalido, tente novamente')
+
+
+    sexo = input('Digite seu sexo: ').lower()
+    if sexo_valido(sexo):
+        print('Sexo validado')
+    else:
+        print('Sexo invalido, tente novamente')
+
+
+    civil = input('Digite seu estado civil: ').lower()
+    if estado_valido (civil):
+        print('Estado validado')
+    else:
+        print('Estado incorreto') 
+    break
