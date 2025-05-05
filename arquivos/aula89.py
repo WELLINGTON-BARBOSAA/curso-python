24522f5eaf37b593196c018f32f376aeed0484a5
Wellington <wgn1910ww@gmail.com>
2025-05-05 20:28:50 -0300

    Dir, hasattr e getattr

---
+++ arquivos/aula89.py
@@ -0,0 +1,13 @@
+# dir, hasattr e getattr em Python
+# string = 'Luiz'
+# metodo = 'strip'
+
+# if hasattr(string, metodo):
+#     print('Existe upper')
+#     print(getattr(string, metodo)())
+# else:
+#     print('Não existe o método', metodo)
+
+tupla = (0, 1, 2, 3, 4)
+
+print(tupla)
