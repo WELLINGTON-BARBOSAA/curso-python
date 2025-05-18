b36aa039cbd377010887da62f5fec6bac174768a
Wellington <wgn1910ww@gmail.com>
2025-05-18 19:25:45 -0300

    codigo melhorado usando funcoes, real em dolar

--- atividades/atv26.py
+++ atividades/atv26.py
@@ -1,3 +1,18 @@
-real = float(input('Digite quantos reais você tem: '))
-formula = real % 5.22
-print(f'Voce tem: US$ {formula:.2f}')
+# real = float(input('Digite quantos reais você tem: '))
+# formula = real / 5.22
+# print(f'Voce tem: US$ {formula:.2f}')
+
+def moeda (real):
+    return real / 5.22
+
+
+while True:
+
+     try: 
+         valor_real = float(input('Digite o valor em real: '))
+         valor_dolar = moeda(valor_real)
+         print(f'Seu valor em dolar é US$ {valor_dolar:.2f}')
+         break
+    
+     except ValueError: 
+         print('Você não digitou um numero, tente novamente')    
