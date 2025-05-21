3a1ec40f48d7e12f918a09a6ebfca193d43ec1f6
Wellington <wgn1910ww@gmail.com>
2025-05-21 19:14:41 -0300

    Atividade sobre desconto, usando funcoes

--- atividades/atv28.py
+++ atividades/atv28.py
@@ -1,7 +1,23 @@
 'Faça um algoritmo que leia um preço e mostre'
 'seu novo preço, com desconto de 5%'
 
-preco = float(input('Digite o valor do preço: '))
-des = preco * 15/100
-resul_des = preco - des
-print(f'O preço original é de {preco}, mas com os desconto de 5% vai fica {resul_des:.2f}')
+# preco = float(input('Digite o valor do preço: '))
+# des = preco * 15/100
+# resul_des = preco - des
+# print(f'O preço original é de {preco}, mas com os desconto de 5% vai fica {resul_des:.2f}')
+
+
+def desconto (preco):
+    return preco *15/100
+
+# print(desconto(300))
+
+while True:
+    try:
+        preco_atual = float(input('Digite o preço do valor: '))
+        preco_desconto = desconto(preco_atual)
+        preco_final = preco_atual - preco_desconto
+        print(f'O produto custa R$ {preco_atual:.2f}, mas com o desconto fica R$ {preco_final:.2f}')    
+        break
+    except ValueError: 
+        print('Voce não digitou um numero, tente novamente') 
