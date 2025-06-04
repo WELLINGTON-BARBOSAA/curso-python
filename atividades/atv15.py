f3fb63e960bd0a0f9a8e2c5011f9977a75d79e44
Wellington <wgn1910ww@gmail.com>
2025-06-04 14:32:54 -0300

    melhorando atv15 com funcoes

--- atividades/atv15.py
+++ atividades/atv15.py
@@ -2,9 +2,29 @@
 # o número de horas trabalhadas no mês. 
 # Calcule e mostre o total do seu salário no referido mês. 
 
-ganho_hora = float(input('Quanto voce ganha por hora: '))
-horas_tbdas = float(input('Digite sua carga horaria por dia: '))
+# ganho_hora = float(input('Quanto voce ganha por hora: '))
+# horas_tbdas = float(input('Digite sua carga horaria por dia: '))
 
-ganho_mensal = (ganho_hora * horas_tbdas) * 30
+# ganho_mensal = (ganho_hora * horas_tbdas) * 30
 
-print(ganho_mensal)
+# print(ganho_mensal)
+
+def trabalho_do_mes (ganho, horas):
+    return (ganho * horas) * 30 
+
+while True:
+    try:
+        ganho_hora = float(input('Digite quanto é seu ganho por hora: '))
+        horas_trabalhadas = float(input('Digite sua carga horaria por dia: '))   
+        
+        if ganho_hora < 0 or horas_trabalhadas < 0 :
+            print('As horas tem que ser positivas, tente novamente')
+            continue
+        
+        
+        ganho_mensal = trabalho_do_mes(ganho_hora,horas_trabalhadas)
+        print(f'Seu ganho mensal é de {ganho_mensal:.2f}')
+        break
+
+    except ValueError:
+        print('Voce não digitou nada')
