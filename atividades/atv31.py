c65089033a475368e2fe8746a3a85ff388eb16db
Wellington <wgn1910ww@gmail.com>
2025-06-04 14:46:56 -0300

    melhorando atv 31 de cateto oposto, adjacente e hipotenusa

--- atividades/atv31.py
+++ atividades/atv31.py
@@ -2,13 +2,22 @@
 do cateto adjacentede uma tringulo retangulo, 
 calcule e mostre o comprimento da hipotenusa'''
 
-import math
+# import math
 
-cateto_oposto = float(input('Digite o valor do cateto aposto: '))
-cateto_adjacente = float(input('Digite o valor do cateto adjacente: '))
-hipotenusa = (cateto_oposto**2) + (cateto_adjacente**2)
-hipotenusa2 = math.sqrt(hipotenusa)
-print(f'Valor da hipotenusa é da raiz de {hipotenusa} é de {hipotenusa2:.2f}')
+# cateto_oposto = float(input('Digite o valor do cateto aposto: '))
+# cateto_adjacente = float(input('Digite o valor do cateto adjacente: '))
+# hipotenusa = (cateto_oposto**2) + (cateto_adjacente**2)
+# hipotenusa2 = math.sqrt(hipotenusa)
+# print(f'Valor da hipotenusa é da raiz de {hipotenusa} é de {hipotenusa2:.2f}')
 
 
 #deixar o codigo mais clean
+
+# import math
+
+# cateto_oposto = float(input('Digite o valor do cateto oposto: '))
+# cateto_adj = float(input('Digite o valor do cateto adjacente: '))
+# hipotenusa = math.hypot(cateto_oposto, cateto_adj)
+# print(f'O valor da hipotenusa é de {hipotenusa:.2f}')
+
+'Fazer uma com funções e tratamento de exceções'
