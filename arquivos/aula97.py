d7a2f1b6499517b396a4de5c07daafa3357246b7
Wellington <wgn1910ww@gmail.com>
2025-06-11 14:27:54 -0300

    introducao a modularizacao - arquivo importado

---
+++ arquivos/aula97.py
@@ -0,0 +1,17 @@
+# Modularização - Entendendo os seus próprios módulos Python
+# O primeiro módulo executado chama-se __main__
+# Você pode importar outro módulo inteiro ou parte do módulo
+# O python conhece a pasta onde o __main__ está e as pastasAdd commentMore actions
+# abaixo dele.
+# Ele não reconhece pastas e módulos acima do __main__ por
+# padrão
+# O python conhece todos os módulos e pacotes presentes
+# nos caminhos de sys.path
+
+import aula97_m
+from aula97_m import soma
+
+
+# print('Este modulo se chama, ', __name__)
+# print(nome)
+print(soma(555,1255))
