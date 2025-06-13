f22f25161c70b769dd43227c073e7182a1bd302e
Wellington <wgn1910ww@gmail.com>
2025-06-13 19:21:58 -0300

    melhorando atv2 com funcao

--- atividades/atv2.py
+++ atividades/atv2.py
@@ -1,5 +1,18 @@
 '''Faça um Programa que peça um número 
 e então mostre a mensagem O número informado foi [número]. '''
 
-numero=input('Digite um numero: ')
-print(f'O numero digitado foi: {numero}')
+# numero=input('Digite um numero: ')
+# print(f'O numero digitado foi: {numero}')
+
+def numero (number):
+    return number
+
+while True:
+    try:
+        num_dig = int(input('Digite um numero: '))
+        digitado = numero(num_dig)
+        print(f'O numero digitado foi {digitado}')
+        break
+    except ValueError:
+        print('Voce não digitou um numero')
+
