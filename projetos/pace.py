636fe781041d09dd688527a22964605212e5bdb4
Wellington <wgn1910ww@gmail.com>
2025-06-30 21:54:25 -0300

    melhorando o código de pace

--- projetos/pace.py
+++ projetos/pace.py
@@ -21,15 +21,4 @@ while True:
     except ValueError:
         print('Voce digitou algo errado, tente novamente')
 
-# tempo = float(input('Insira seu tempo: '))
-# distancia_km = float(input('Insira a distância: '))
-
-# calculo_pace = tempo/distancia_km
-
-# tempo_em_horas = tempo/60
-# velocidade_media = distancia_km/tempo_em_horas
-
-# print(f'Seu pace é de : {calculo_pace:.2f} min/Km')
-# print(f'E sua velocidade média é de: {velocidade_media:.2f} Km/h')
-
-    
+# melhorar o codigo usando funções
