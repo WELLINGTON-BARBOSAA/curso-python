b54b6b8145c593da03b3d11b5463e72d9dabf182
Wellington <wgn1910ww@gmail.com>
2025-09-03 21:27:29 -0300

    correção do calculo da multa

--- atividades/atv38.py
+++ atividades/atv38.py
@@ -3,12 +3,13 @@ Se ele ultrapassar 80km/h, mostre a mensagem dizendo que foi multado
 A multa vai custar R$ 7,00 por km acima do limite'''
 
 vel_carro = int(input('Digite a velocidade do carro: '))
-multa = vel_carro * 7
-print(f'Sua velocidade é de {vel_carro}Km')
+multa = (vel_carro - 80) * 7
+print(f'Sua velocidade é de {vel_carro}Km/h')
 if vel_carro < 80 :
     print('Sua velocidade esta normal')
 
 else:
     print('MULTADO COM SUCESSO')
-    print(f'Sua multa é de R${multa}')
+    print(f'Sua multa é de R$ {multa}')
     
+# fazer essa atividade usando funções
