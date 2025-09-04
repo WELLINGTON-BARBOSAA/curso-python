9522572d26d471c7dd485868d344636cc409d2b8
Wellington <wgn1910ww@gmail.com>
2025-09-03 21:17:57 -0300

    Velocidade carro

---
+++ atividades/atv38.py
@@ -0,0 +1,14 @@
+''' Escreva um programa que leia a velocidade de um carro
+Se ele ultrapassar 80km/h, mostre a mensagem dizendo que foi multado
+A multa vai custar R$ 7,00 por km acima do limite'''
+
+vel_carro = int(input('Digite a velocidade do carro: '))
+multa = vel_carro * 7
+print(f'Sua velocidade é de {vel_carro}Km')
+if vel_carro < 80 :
+    print('Sua velocidade esta normal')
+
+else:
+    print('MULTADO COM SUCESSO')
+    print(f'Sua multa é de R${multa}')
+    
