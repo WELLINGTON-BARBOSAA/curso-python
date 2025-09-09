8eca87f5315563f50d9521741842a611c2cf8b36
Wellington <wgn1910ww@gmail.com>
2025-09-09 20:35:57 -0300

    Deixando mais clean o codigo

--- atividades/atv40.py
+++ atividades/atv40.py
@@ -3,16 +3,11 @@
 'cobrando R$ 0.50 por km para viagens de até 200km e R$ 0.45 para as viagens mais longas'
 
 distancia = int(input('Qual a distância da viagem: '))
-km_menor = distancia * 0.50
-km_maior = distancia * 0.45
+preco_total = distancia * 0.50
+preco_desc = distancia * 0.45
 
 if distancia <= 200:
-    print(f'Sua distancia foi de {distancia} Km ')
-    print(f'O valor da viagem vai custar {km_menor}')
-
-if distancia > 200:
-    print(f'Sua distancia foi de {distancia} Km')
-    print(f'O valor da viagem com desconto é de {km_maior}')
+    print(f'A distancia foi de {distancia}, no valor de  R$ {preco_total:.2f}')
 
 else:
-    print('Você não digitou nada')
+    print(f'A distância foi {distancia}, no valor de R$ {preco_desc:.2f}')
