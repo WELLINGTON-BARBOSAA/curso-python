7b32ea3cb5f68d0680b5cc882c2fd5c031b6b382
Wellington <wgn1910ww@gmail.com>
2025-11-19 20:49:37 -0300

    Mapa mental, dia 7 do 21 dias

---
+++ python/aula5.py
@@ -0,0 +1,63 @@
+'''
+====================================================
+     MAPA MENTAL – SEMANA 1  - Desafio de 21 dias
+   LÓGICA DE PROGRAMAÇÃO
+====================================================
+
+Lógica de Programação — Semana 1
+
+--------------------------------
+1) VARIÁVEIS
+Guardam valores
+Podem mudar
+ Exemplos:
+    nome = "Wellington"
+    idade = 30
+
+--------------------------------
+2) TIPOS DE DADOS
+ int  - números inteiros
+ float - números de ponto flutuante
+ str - textos
+ bool - True ou False
+
+--------------------------------
+3) OPERADORES
+Aritméticos:
+  +  -  *  /  //  %  **
+
+Comparação:
+  == != > < >= <=
+
+Lógicos:
+  and  or  not
+
+--------------------------------
+4) ENTRADA E SAÍDA
+ input(): recebe dados
+ print(): exibe dados
+ Exemplo:
+    idade = int(input("Digite a idade: "))
+    print(idade)
+
+--------------------------------
+5) CONDICIONAIS
+ Estruturas de decisão
+ if / elif / else
+ Exemplo:
+    if nota >= 7:
+        print("Aprovado")
+    else:
+        print("Reprovado")
+
+--------------------------------
+6) LOOPS
+For:
+ Repetição contada
+ for n in range(1, 11):
+
+While:
+Repetição até parar
+while senha != "123":
+
+'''
