171638aabb439bc53a41ebd699a83b91abb26a9f
Wellington <wgn1910ww@gmail.com>
2025-12-22 20:56:41 -0300

    apagando arquivo

--- proj_cdc21/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-ar_removida = int(input('Digite qual tarefa deseja remover: '))
-        if tar_removida.isnumeric() == False:
-            print('Digite o numero ao lado da tarefa para remove-la')
-            continue
