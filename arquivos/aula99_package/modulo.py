a0f3111f92ba85c7497656a4714ca91a1e352652
Wellington <wgn1910ww@gmail.com>
2026-02-13 22:53:30 -0300

    introducao aos package

---
+++ arquivos/aula99_package/modulo.py
@@ -0,0 +1,14 @@
+__all__ = [
+    'variavel',
+    'soma_modulo',
+]
+
+variavel = 'alguma coisa'
+
+def soma_modulo(x,y):
+    return x + y
+
+
+
+# comando all só funciona se caso o usuario fazer o seguinte comando:
+# 
