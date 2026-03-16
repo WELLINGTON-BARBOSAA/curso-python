4bbe520b66d18f220ef75a6287b99f472a649388
Wellington <wgn1910ww@gmail.com>
2026-03-16 20:31:15 -0300

    aula sobre import lib

---
+++ arquivos/aula98.py
@@ -0,0 +1,16 @@
+
+# import importlib
+
+# import aula98_m
+
+
+# print(aula98_m.nome)
+
+# for i in range(5):
+#     importlib.reload(aula98_m)
+#     print(i)
+
+# print('Fim')
+
+usuario = int(input('Digite um numero: '))
+print(usuario)
