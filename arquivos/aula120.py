1473c0291b5009a1e69f71acd7d449ffe95572de
Wellington <wgn1910ww@gmail.com>
2026-07-21 18:28:05 -0300

    Introdução a POO

--- arquivos/aula120.py
+++ arquivos/aula120.py
@@ -10,15 +10,35 @@
 # print(isinstance(string, str))
 
 class Pessoa:
-    def __init__(self, nome, sobrenome):
+    def __init__(self, nome, sobrenome, bairro, cidade):
         self.nome = nome
         self.sobrenome = sobrenome
+        self.bairro = bairro
+        self.cidade = cidade
 
-p1 = Pessoa('Wellington', 'Barbosa')
-p2 = Pessoa('Eduarda', 'Porto')
-
+p1 = Pessoa('Wellington', 'Barbosa', 'Morro Doce', 'São Paulo')
 print(p1.nome)
 print(p1.sobrenome)
+print(p1.bairro)
+print(p1.cidade)
+print()
 
+p2 = Pessoa('Eduarda', 'Porto', 'Centro', 'Guarulhos')
 print(p2.nome)
 print(p2.sobrenome)
+print(p2.bairro)
+print(p2.cidade)
+print()
+
+p3 = Pessoa('Jose', 'Fialho', 'Centro', 'São João do Piaui')
+print(p3.nome)
+print(p3.sobrenome)
+print(p3.bairro)
+print(p3.cidade)
+print()
+
+p4 = Pessoa('Raimunda', 'de Sousa', 'São Sebastião', 'sSão João do Piaui')
+print(p4.nome)
+print(p4.sobrenome)
+print(p4.bairro)
+print(p4.cidade)
