e32d69cd77eab5ab1de83c3c6c9dce4d6ed45aa7
Wellington <wgn1910ww@gmail.com>
2026-07-20 21:52:27 -0300

    Entendendo metodo, init e self dentro da classe

--- arquivos/aula120.py
+++ arquivos/aula120.py
@@ -8,17 +8,14 @@
 # string = 'Luiz'  # str
 # print(string.upper())
 # print(isinstance(string, str))
+
 class Pessoa:
-    ...
+    def __init__(self, nome, sobrenome):
+        self.nome = nome
+        self.sobrenome = sobrenome
 
-
-p1 = Pessoa('Luiz', 'Otávio')
-p1.nome = 'Luiz'
-p1.sobrenome = 'Otávio'
-
-p2 = Pessoa('Maria', 'Joana')
-p2.nome = 'Maria'
-p2.sobrenome = 'Joana'
+p1 = Pessoa('Wellington', 'Barbosa')
+p2 = Pessoa('Eduarda', 'Porto')
 
 print(p1.nome)
 print(p1.sobrenome)
